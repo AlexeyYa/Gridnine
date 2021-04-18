a6de2a2429d270190e4f7abb4ddf9c9cb9bbc0c9
Alexey <l337.cat@gmail.com>
2021-04-18 13:08:39 +0300

    Project upload

---
+++ ConsoleApp1/FlightFilter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Gridnine.FlightCodingTest
+{
+    public interface IFlightFilter
+    {
+        public bool Check(Flight flight);
+    }
+
+    public class FlightBeforeDateTimeFilter : IFlightFilter
+    {
+        private DateTime dateTime;
+        private bool isTrueBefore;
+        public FlightBeforeDateTimeFilter(DateTime dateTime, bool isTrueBefore=true)
+        {
+            this.dateTime = dateTime;
+            this.isTrueBefore = isTrueBefore;
+        }
+        public bool Check(Flight flight)
+        {
+            //assert(flight.Segments.Count > 0);
+
+            if (flight.Segments[0].DepartureDate < dateTime)
+                return isTrueBefore;
+            else
+                return !isTrueBefore;
+        }
+    }
+
+    public class FlightValidateSegmentsFilter : IFlightFilter
+    {
+        public FlightValidateSegmentsFilter() { }
+        public bool Check(Flight flight)
+        {
+            foreach (var segment in flight.Segments)
+                if (segment.ArrivalDate < segment.DepartureDate)
+                    return false;
+
+            return true;
+        }
+    }
+
+    public class FlightLandTimeFilter : IFlightFilter
+    {
+        private TimeSpan timeDifference;
+
+        public FlightLandTimeFilter(TimeSpan timeDifference)
+        {
+            this.timeDifference = timeDifference;
+        }
+        public bool Check(Flight flight)
+        {
+            for (int i = 1; i < flight.Segments.Count; i++)
+            {
+                if (flight.Segments[i].DepartureDate - flight.Segments[i - 1].ArrivalDate > timeDifference)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public class FilterCollection
+    {
+        private List<IFlightFilter> flightFilters;
+
+        public FilterCollection()
+        {
+            flightFilters = new List<IFlightFilter>();
+        }
+        public void AddFilter(IFlightFilter flightFilter)
+        {
+            flightFilters.Add(flightFilter);
+        }
+        private bool Check(Flight flight)
+        {
+            foreach (var filter in flightFilters)
+                if (!filter.Check(flight))
+                    return false;
+
+            return true;
+        }
+        public List<Flight> GetFilteredList(IEnumerable<Flight> flights)
+        {
+            List<Flight> result = new List<Flight>();
+            foreach (var flight in flights)
+            {
+                if (Check(flight))
+                    result.Add(flight);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gridnine.FlightCodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fb = new Gridnine.FlightCodingTest.FlightBuilder();
+            var flights = fb.GetFlights();
+
+            FilterCollection filterCollection1 = new FilterCollection();
+            filterCollection1.AddFilter(new FlightBeforeDateTimeFilter(DateTime.Now, false));
+
+            FilterCollection filterCollection2 = new FilterCollection();
+            filterCollection2.AddFilter(new FlightValidateSegmentsFilter());
+
+            FilterCollection filterCollection3 = new FilterCollection();
+            filterCollection3.AddFilter(new FlightLandTimeFilter(new TimeSpan(2, 0, 0)));
+
+            var filtered1 = filterCollection1.GetFilteredList(flights);
+            var filtered2 = filterCollection2.GetFilteredList(flights);
+            var filtered3 = filterCollection3.GetFilteredList(flights);
+
+            PrintFlights(flights);
+            Console.WriteLine("\nFilter 1");
+            PrintFlights(filtered1);
+            Console.WriteLine("\nFilter 2");
+            PrintFlights(filtered2);
+            Console.WriteLine("\nFilter 3");
+            PrintFlights(filtered3);
+        }
+
+        static void PrintFlights(IEnumerable<Flight> flights)
+        {
+            foreach (var flight in flights)
+            {
+                foreach (var segment in flight.Segments)
+                    Console.Write(segment.DepartureDate.ToString("g") + " - " + segment.ArrivalDate.ToString("g") + "; ");
+                Console.Write('\n');
+            }
+        }
+    }
+}
